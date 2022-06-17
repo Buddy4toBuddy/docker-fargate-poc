73b8ceb283a633fa2e39d7616eff38b8a6a53f1e
Vimal Lal V <81673118+Buddy4toBuddy@users.noreply.github.com>
2022-06-17 13:35:16 +0530

    cahnges

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello this is version-1 POC"));
+app.get("/", (req, res) => res.send("Hello this is version-2 POC"));
 
 app.get("/health", (req, res) => {
   res.status(200);
